d97ea9779326eb9305d39d73657fb4f113e3843d
Brian Xu <201601067@iacademy.edu.ph>
2020-04-16 17:31:00 +0800

    Fix bug

--- penny_pincher/settings.py
+++ penny_pincher/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-
+from django.urls import reverse_lazy
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -27,7 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 AUTH_USER_MODEL = 'user.User'
-
+LOGIN_REDIRECT_URL = '/admin'
 # Application definition
 
 INSTALLED_APPS = [
