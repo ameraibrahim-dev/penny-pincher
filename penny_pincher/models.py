99064e989e7e1b07c91c6ef50a018a05541f4b3d
Brian Xu <201601067@iacademy.edu.ph>
2020-04-16 20:08:15 +0800

    nullable notes

--- penny_pincher/models.py
+++ penny_pincher/models.py
@@ -6,7 +6,7 @@ class AbstractTransaction(models.Model):
     amount = MoneyField(max_digits=14, decimal_places=2, default_currency='PHP')
     date = models.DateField()
     isExpense = models.BooleanField(null=False, blank=False)
-    note = models.CharField(max_length=3000)
+    note = models.CharField(max_length=3000,null=True,blank=True)
 
     class Meta:
         abstract = True
