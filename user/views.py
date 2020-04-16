828c6fab98b406aca7e78cea8ccd922c0336bbf3
Brian Xu <201601067@iacademy.edu.ph>
2020-04-16 18:02:45 +0800

    add user profile

--- user/views.py
+++ user/views.py
@@ -1,17 +1,18 @@
-from django.contrib.auth import authenticate
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import UpdateView
 from django_registration.backends.activation.views import RegistrationView
 from .forms import UserRegistrationForm
-from django_registration import signals
 from django.urls import reverse_lazy
-from django.contrib.auth.views import PasswordChangeView, PasswordResetView,PasswordResetConfirmView
-
+from django.contrib.auth.views import PasswordChangeView, PasswordResetView, PasswordResetConfirmView
 
 # Create your views here.
+from .models import User
+
+
 class RegistrationView(RegistrationView):
-    email_body_template='django_registration/activation_email_body.html'
+    email_body_template = 'django_registration/activation_email_body.html'
     form_class = UserRegistrationForm
     success_url = reverse_lazy("users:django_registration_complete")
-
 
 
 class PasswordChangeView(PasswordChangeView):
@@ -25,6 +26,18 @@ class PasswordResetView(PasswordResetView):
     success_url = reverse_lazy('users:password_reset_done')
     template_name = 'user_auth/password_reset_form.html'
 
+
 class PasswordResetConfirmView(PasswordResetConfirmView):
     success_url = reverse_lazy('users:password_reset_complete')
     template_name = 'user_auth/password_reset_confirm.html'
+
+
+class UserUpdateView(UpdateView, LoginRequiredMixin):
+    fields = ['first_name', 'last_name']
+    template_name = 'user/user_profile.html'
+
+    def get_success_url(self):
+        return reverse_lazy('users:profile')
+
+    def get_object(self):
+        return User.objects.get(pk=self.request.user.pk)
