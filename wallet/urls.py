78a224445a2ea6501fc17dc3b55f0a946540c926
Brian Xu <201601067@iacademy.edu.ph>
2020-04-16 21:20:47 +0800

    add category actions

--- wallet/urls.py
+++ wallet/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path, include, reverse_lazy
-from .views import CustomCategoryListView, DefinedEarningsCategories, DefinedExpensesCategories
+from .views import CustomCategoryListView, DefinedExpensesCategories, DefinedEarningsCategories, \
+    DeleteCustomCategoryView, UpdateCustomCategoryView, CreateCustomCategoryView
 
 app_name = "wallet"
 
 urlpatterns = [
+    # categories
     path('custom/category/list', CustomCategoryListView.as_view(), name='custom_categories_list'),
-    path('custom/category/create', CustomCategoryListView.as_view(), name='create_custom_categories'),
-    path('custom/category/update', CustomCategoryListView.as_view(), name='update_custom_categories_'),
-    path('custom/category/delete', CustomCategoryListView.as_view(), name='delete_custom_categories_list'),
+    path('custom/category/create', CreateCustomCategoryView.as_view(), name='create_custom_categories'),
+    path('custom/category/update', UpdateCustomCategoryView.as_view(), name='update_custom_categories_'),
+    path('custom/category/delete', DeleteCustomCategoryView.as_view(), name='delete_custom_categories_list'),
     path('predefined/earnings/category/list', DefinedEarningsCategories.as_view(),
          name='predefined_earnings_categories_list'),
     path('predefined/expenses/category/list', DefinedExpensesCategories.as_view(),
--- wallet/views/category.py
+++ wallet/views/category.py
@@ -11,38 +11,50 @@ class CustomCategoryListView(LoginRequiredMixin,ListView):
     paginate_by = 30
 
     def get_queryset(self):
-        return Category.objects.filter(owner=self.request.user, is_custom=False)
+        return Category.objects.filter(owner=self.request.user, is_custom=True)
 
 
-class CreateCategoryListView(LoginRequiredMixin,CreateView):
+class CreateCustomCategoryView(LoginRequiredMixin,CreateView):
     model = Category
     fields = ['name']
-    success_url = reverse_lazy('wallet:')
+    success_url = reverse_lazy('wallet:custom_categories_list')
 
 
-class UpdateCategoryListView(LoginRequiredMixin,ListView):
-    paginate_by = 30
+class UpdateCustomCategoryView(LoginRequiredMixin,ListView):
+    model = Category
+    fields = ['name']
+    success_url = reverse_lazy('wallet:custom_categories_list')
+
+    def form_valid(self, form):
+        instance = form.save(commit=False)
+        instance.owner = self.request.user
+        return super(UpdateCustomCategoryView, self).form_valid(form)
+
+    def get_context_data(self, **kwargs):
+        ctx = super(UpdateCustomCategoryView, self).get_context_data(**kwargs)
+        ctx['title'] = 'Update Store'
+        return ctx
 
     def get_queryset(self):
-        return Category.objects.filter(owner=self.request.user, is_custom=False)
+        return Category.objects.filter(owner=self.request.user,is_custom=True)
 
 
-class DeleteCategoryListView(LoginRequiredMixin,ListView):
-    paginate_by = 30
+class DeleteCustomCategoryView(LoginRequiredMixin,ListView):
+    success_url = reverse_lazy('wallet:custom_categories_list')
 
     def get_queryset(self):
-        return Category.objects.filter(owner=self.request.user, is_custom=False)
+        return Category.objects.filter(owner=self.request.user, is_custom=True)
 
 
 class DefinedExpensesCategories(LoginRequiredMixin,ListView):
     paginate_by = 30
 
     def get_queryset(self):
-        return get_predefined_expenses_categories(self.user)
+        return get_predefined_expenses_categories(self.request.user)
 
 
 class DefinedEarningsCategories(LoginRequiredMixin,ListView):
     paginate_by = 30
 
     def get_queryset(self):
-        return get_predefined_earnings_categories(self.user)
+        return get_predefined_earnings_categories(self.request.user)
