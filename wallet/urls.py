c2a4ff0c879e408cb93df30d90a671a5bbb4721f
Brian Xu <201601067@iacademy.edu.ph>
2020-04-16 21:14:19 +0800

    rename urls app name

--- wallet/urls.py
+++ wallet/urls.py
@@ -4,8 +4,10 @@ from .views import CustomCategoryListView, DefinedEarningsCategories, DefinedExp
 app_name = "wallet"
 
 urlpatterns = [
-    # login,logout
     path('custom/category/list', CustomCategoryListView.as_view(), name='custom_categories_list'),
+    path('custom/category/create', CustomCategoryListView.as_view(), name='create_custom_categories'),
+    path('custom/category/update', CustomCategoryListView.as_view(), name='update_custom_categories_'),
+    path('custom/category/delete', CustomCategoryListView.as_view(), name='delete_custom_categories_list'),
     path('predefined/earnings/category/list', DefinedEarningsCategories.as_view(),
          name='predefined_earnings_categories_list'),
     path('predefined/expenses/category/list', DefinedExpensesCategories.as_view(),
--- wallet/views/category.py
+++ wallet/views/category.py
@@ -1,25 +1,47 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView
 
 from wallet.models import Category
 from wallet.utils import get_predefined_expenses_categories, get_predefined_earnings_categories
 
 
-class CustomCategoryListView(ListView):
+class CustomCategoryListView(LoginRequiredMixin,ListView):
     paginate_by = 30
 
     def get_queryset(self):
         return Category.objects.filter(owner=self.request.user, is_custom=False)
 
 
-class DefinedExpensesCategories(ListView):
+class CreateCategoryListView(LoginRequiredMixin,CreateView):
+    model = Category
+    fields = ['name']
+    success_url = reverse_lazy('wallet:')
+
+
+class UpdateCategoryListView(LoginRequiredMixin,ListView):
+    paginate_by = 30
+
+    def get_queryset(self):
+        return Category.objects.filter(owner=self.request.user, is_custom=False)
+
+
+class DeleteCategoryListView(LoginRequiredMixin,ListView):
+    paginate_by = 30
+
+    def get_queryset(self):
+        return Category.objects.filter(owner=self.request.user, is_custom=False)
+
+
+class DefinedExpensesCategories(LoginRequiredMixin,ListView):
     paginate_by = 30
 
     def get_queryset(self):
         return get_predefined_expenses_categories(self.user)
 
 
-class DefinedEarningsCategories(ListView):
+class DefinedEarningsCategories(LoginRequiredMixin,ListView):
     paginate_by = 30
 
     def get_queryset(self):
