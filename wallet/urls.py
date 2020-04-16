7c69a7ff094fb34d55cc5fc90ac0041df82a1a3a
Brian Xu <201601067@iacademy.edu.ph>
2020-04-16 21:03:02 +0800

    add list

--- wallet/urls.py
+++ wallet/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path, include, reverse_lazy
+from .views import CustomCategoryListView, DefinedEarningsCategories, DefinedExpensesCategories
+
 app_name = "wallet"
 
 urlpatterns = [
     # login,logout
-    path('login/', LoginView.as_view(template_name='user_auth/login.html'), name='login'),
+    path('custom/category/list', CustomCategoryListView.as_view(), name='custom_categories_list'),
+    path('predefined/earnings/category/list', DefinedEarningsCategories.as_view(),
+         name='predefined_earnings_categories_list'),
+    path('predefined/expenses/category/list', DefinedExpensesCategories.as_view(),
+         name='predefined_expenses_categories_list'),
 ]
