0b5ccb53fcf84571fc408e71896890a9dbeaa120
Brian Xu <201601067@iacademy.edu.ph>
2020-04-18 16:34:59 +0800

    adjust category app

--- category/urls.py
+++ category/urls.py
@@ -5,7 +5,7 @@ app_name = "category"
 
 urlpatterns = [
     # categories
-    path('custom/list/', CustomCategoryListView.as_view(), name='custom_categories_list'),
+    path('custom/list/', CustomCategoryListView.as_view(), name='custom_category_list'),
     path('custom/create/', CreateCustomCategoryView.as_view(), name='create_custom_category'),
     path('custom/update/<int:pk>/', UpdateCustomCategoryView.as_view(), name='update_custom_category'),
     path('custom/delete/<int:pk>/', DeleteCustomCategoryView.as_view(), name='delete_custom_category'),
