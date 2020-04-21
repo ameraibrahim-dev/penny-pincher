38a1ca572f44fc41f99319becd70caf4c4651d89
Brian Xu <201601067@iacademy.edu.ph>
2020-04-21 15:14:48 +0800

    update api url naming

--- api/urls.py
+++ api/urls.py
@@ -4,12 +4,12 @@ from .views import *
 app_name = "api"
 
 urlpatterns = [
-    path('user/expenses/categories/', AllExpenseCategoryJsonList.as_view(),
+    path('v1/user/expenses/categories/', AllExpenseCategoryJsonList.as_view(),
          name='expense_categories_by_user'),
-    path('user/earnings/categories/', AllEarningsCategoryJsonList.as_view(),
+    path('v1/user/earnings/categories/', AllEarningsCategoryJsonList.as_view(),
          name='earnings_categories_by_user'),
-    path('user/used/categories/', AllUsedCategoryJsonList.as_view(),
+    path('v1/user/used/categories/', AllUsedCategoryJsonList.as_view(),
          name='used_categories'),
-    path('user/wallet/transactions/list/', WalletTransactionsByUser.as_view(),
+    path('v1/user/wallet/transactions/list/', WalletTransactionsByUser.as_view(),
          name='wallet_transactions_by_user'),
 ]
