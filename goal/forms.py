5c2c3c650e8aa7e09330b18f1b0ce2bab5c53c73
Brian Xu <201601067@iacademy.edu.ph>
2020-04-23 18:16:07 +0800

    Add validator

--- goal/forms.py
+++ goal/forms.py
@@ -1,16 +1,20 @@
+from django.core.validators import MinValueValidator
 from django.forms import ModelForm
 from django import forms
+from djmoney.forms import MoneyField
+from djmoney.models.validators import MinMoneyValidator
 
 from goal.models import Goal
 
 
 class CreateGoalForm(ModelForm):
-    target_date=forms.DateField(widget=forms.DateInput(attrs={'type':'date'}))
+    target_date = forms.DateField(widget=forms.DateInput(attrs={'type': 'date'}))
+
     class Meta:
         model = Goal
-        fields = ['name', 'balance', 'target_amount','target_date']
+        fields = ['name', 'balance', 'target_amount', 'target_date']
         labels = {
-            'name':'What is the name of your goal',
+            'name': 'What is the name of your goal',
             'target_amount': 'target_amount',
             'balance': 'Initial Amount',
             'target_date': 'Ending Date',
@@ -18,10 +22,11 @@ class CreateGoalForm(ModelForm):
 
 
 
+
 class UpdateGoalForm(ModelForm):
     class Meta:
         model = Goal
-        fields = ['name','target_amount','target_date']
+        fields = ['name', 'target_amount', 'target_date']
         labels = {
             'What is the name of your goal': 'name',
             'How much do you want to save': 'target_amount',
--- goal/models.py
+++ goal/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 from djmoney.models.fields import MoneyField
 from datetime import date
+
+from djmoney.models.validators import MinMoneyValidator
+from moneyed import Money
+
 from penny_pincher.models import AbstractTransaction
 from user.models import User
 from decimal import Decimal
@@ -8,8 +12,10 @@ from decimal import Decimal
 
 class Goal(models.Model):
     name = models.CharField(max_length=500)
-    balance = MoneyField(max_digits=14, decimal_places=2, default_currency='PHP')
-    target_amount = MoneyField(max_digits=14, decimal_places=2, default_currency='PHP')
+    balance = MoneyField(max_digits=14, decimal_places=2, default_currency='PHP',
+                         validators=[MinMoneyValidator(0)])
+    target_amount = MoneyField(max_digits=14, decimal_places=2, default_currency='PHP',
+                               validators=[MinMoneyValidator(1)])
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     target_date = models.DateField()
 
