14788ee9200477f36fcab549de7b4e8f17c851dd
Brian Xu <201601067@iacademy.edu.ph>
2020-04-23 17:09:18 +0800

    add urls

--- goal/models.py
+++ goal/models.py
@@ -1,19 +1,32 @@
 from django.db import models
 from djmoney.models.fields import MoneyField
-
+from datetime import date
 from penny_pincher.models import AbstractTransaction
 from user.models import User
+from decimal import Decimal
 
 
 class Goal(models.Model):
+    name = models.CharField(max_length=500)
     balance = MoneyField(max_digits=14, decimal_places=2, default_currency='PHP')
-    progress = models.FloatField(max)
     target_amount = MoneyField(max_digits=14, decimal_places=2, default_currency='PHP')
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     target_date = models.DateField()
 
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+
+    @property
+    def saving_progress(self):
+        progress = Decimal(self.balance / self.target_amount)
+        progress = round(progress, 2)
+        return progress if progress < 100 else 100
+
+    @property
+    def date_progress(self):
+        progress = Decimal(date.today() / self.target_date)
+        progress = round(progress, 2)
+        return progress if progress < 100 else 100
 
 
 class GoalTransaction(AbstractTransaction):
--- goal/urls.py
+++ goal/urls.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
+from .view import *
 
 app_name = 'goal'
 urlpatterns = [
+    # goals
+    path('list/', GoalListView.as_view(), name='goal_list'),
+    path('create/', CreateGoalView.as_view(), name='create_goal'),
+    path('update/<int:pk>', UpdateGoalView.as_view(), name='update_goal'),
+    path('delete/<int:pk>', DeleteGoalView.as_view(), name='delete_goal'),
+    path('detail/<int:pk>', GoalDetailView.as_view(), name='goal_detail'),
 
 ]
