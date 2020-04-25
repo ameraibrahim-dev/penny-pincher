215115adcb4a1e876213c50d0f34396d519a3491
Brian Xu <201601067@iacademy.edu.ph>
2020-04-25 13:56:03 +0800

    add goal in index back end

--- penny_pincher/view.py
+++ penny_pincher/view.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import TemplateView
 
+from goal.models import Goal, GoalTransaction
 from wallet.models import Wallet, WalletTransaction
 
 
@@ -8,6 +9,7 @@ class DashboardView(LoginRequiredMixin, TemplateView):
     template_name = "index.html"
 
     def get_context_data(self, **kwargs):
+        # goal,wallet
         context = super().get_context_data(**kwargs)
         order_by_list = ['-updated', '-created']  # order desc
         # get latest wallet
@@ -17,6 +19,14 @@ class DashboardView(LoginRequiredMixin, TemplateView):
         else:
             # extract wallet from transaction
             wallet = wallet.wallet
-
         context['wallet'] = wallet
+
+        # Get Latest Goal
+        goal = GoalTransaction.objects.filter(goal__owner=self.request.user).order_by(*order_by_list).first()
+        if not goal:
+            goal = Goal.objects.filter(owner=self.request.user).order_by(*order_by_list).first()
+        else:
+            # extract goal from transaction
+            goal = goal.goal
+        context['goal'] = goal
         return context
