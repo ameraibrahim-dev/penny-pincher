88ad4848bb6fda94bbb8f720540cd6107ebb30bf
Brian Xu <201601067@iacademy.edu.ph>
2020-04-27 08:02:09 +0800

    change var name to be more specific

--- user/views.py
+++ user/views.py
@@ -21,15 +21,14 @@ class LoginView(LoginView):
 class RegistrationView(RegistrationView):
     email_body_template = 'django_registration/activation_email_body.html'
     form_class = UserRegistrationForm
-    success_url = 'django_registration/registration_complete.html'
+    success_template = 'django_registration/registration_complete.html'
 
     def form_valid(self, form):
         context = self.get_context_data()
         form = context.get('form')
         email = form.cleaned_data['email']
-        context = {}
-        context['email'] = email
-        return render(self.request, self.get_success_url(), context=context)
+        success_context = {'email': email}
+        return render(self.request, self.success_template, context=success_context)
 
 
 class PasswordChangeView(PasswordChangeView):
