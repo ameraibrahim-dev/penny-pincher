804ee92da6421b13a6e11247e4c5ca050dee9cc9
Brian Xu <201601067@iacademy.edu.ph>
2020-05-07 13:07:23 +0800

    fix typo

--- wallet/static/js/wallet-transaction-form.js
+++ wallet/static/js/wallet-transaction-form.js
@@ -2,7 +2,7 @@ let categories = [];
 const TYPE_FIELD_LOCATOR = "#id_is_expense";
 const CATEGORY_FIELD_LOCATOR = "#id_category";
 const EARNING_CATEGORIES_API_URL = "/api/v1/user/earnings/categories/";
-const EXPENSE_CATEGOREIS_API_URL = "/api/v1/user/expenses/categories/";
+const EXPENSE_CATEGORIES_API_URL = "/api/v1/user/expenses/categories/";
 // on type change
 $(TYPE_FIELD_LOCATOR).change(
     function () {
@@ -20,7 +20,7 @@ function getCategories(type) {
             url = EARNING_CATEGORIES_API_URL;
             break;
         case "expense":
-            url = EXPENSE_CATEGOREIS_API_URL;
+            url = EXPENSE_CATEGORIES_API_URL;
             break;
         default:
             categories = [];
