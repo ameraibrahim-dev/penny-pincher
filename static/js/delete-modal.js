7c9efba0e527f73ccc0a42976d9feff3c7d61e4c
Lorenzo Ocampo <enzoxo.ocampo7@yahoo.com>
2020-05-10 16:29:23 +0800

    Added Modal, Modal now works in Transaction list, Category list, update wallet and Goals list

---
+++ static/js/delete-modal.js
@@ -0,0 +1,23 @@
+target_class('.transaction-delete');
+target_class('.delete-wallet-link');
+target_class('.category-delete');
+
+
+$('#modal-cancel').on('click', function(){
+
+    $('#delete-modal').removeClass('-active');
+    $('#modal-overlay').removeClass('-apply-overlay');
+
+});
+
+function target_class(trgt_class){
+    $(trgt_class).on('click', function(){
+        $('#delete-modal').toggleClass('-active');
+        $('#modal-overlay').toggleClass('-apply-overlay');
+    });
+}
+
+function delete_modal(link,msg){
+    $('#modal-message').text(msg);
+    $('#modal-delete').attr("href",link)
+}
