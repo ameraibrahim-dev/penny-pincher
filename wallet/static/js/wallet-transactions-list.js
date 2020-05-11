3a51b3f8ff63f8a38eb059227a65d004535cf8b4
Brian Xu <201601067@iacademy.edu.ph>
2020-05-11 21:49:17 +0800

    change event

--- wallet/static/js/wallet-transactions-list.js
+++ wallet/static/js/wallet-transactions-list.js
@@ -1,4 +1,4 @@
-const NOTE_FIELD_LOCATOR="#filterByNoteTextField";
+const NOTE_FIELD_LOCATOR = "#filterByNoteTextField";
 let options = {
     valueNames: ['name', 'note', 'amount'],
     page: 5,
@@ -6,12 +6,13 @@ let options = {
         name: "pagination",
         paginationClass: "pagination",
         outerWindow: 2,
-        innerWindow:2,
+        innerWindow: 2,
 
     }],
 };
 
 let transaction_list = new List('transactions-list', options);
-$( NOTE_FIELD_LOCATOR ).change(function() {
- transaction_list.search($(NOTE_FIELD_LOCATOR).val(), ['note']);
+$(NOTE_FIELD_LOCATOR).keydown(function () {
+    console.log("on change");
+    transaction_list.search($(NOTE_FIELD_LOCATOR).val(), ['note']);
 });
