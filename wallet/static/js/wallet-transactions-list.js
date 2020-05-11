196199c4ff0285435123220a4e91498f710d1952
Brian Xu <201601067@iacademy.edu.ph>
2020-05-11 21:17:09 +0800

    init list js

--- wallet/static/js/wallet-transactions-list.js
+++ wallet/static/js/wallet-transactions-list.js
@@ -1,5 +1,7 @@
 let options = {
-    valueNames: ['name', 'note', 'amount']
+    valueNames: ['name', 'note', 'amount'],
+     page: 3,
+    pagination: true
 };
 
 let hackerList = new List('transactions-list', options);
