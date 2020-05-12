32a7affce068ed934a5aa73c98c94d859c7fe018
Brian Xu <201601067@iacademy.edu.ph>
2020-05-12 23:50:32 +0800

    now can get info

--- wallet/static/js/wallet-overview.js
+++ wallet/static/js/wallet-overview.js
@@ -1,9 +1,23 @@
 {
+    const WALLET_TRANSACTIONS_API_URL = "/api/v1/user/wallet/transactions/list/";
+    const WALLET_LIST_API_URL = "/api/v1/user/wallet/list/";
     const NOTE_FIELD_LOCATOR = "#filterByNoteTextField";
     const DATE_RANGE_FIELD_LOCATOR = "#DateRangeFilter";
     const TOTAL_PERIOD_EXPENSES_TEXT_LOCATOR = "#total-period-expenses";
     const TOTAL_PERIOD_EARNINGS_TEXT_LOCATOR = "#total-period-earnings";
+
+    const WALLET_PK = $("#walletID").val();
     let categories = [];
+    let transactions = [];
+    let totalPeriodExpenses = 0;
+    let totalPeriodEarnings = 0;
+    let walletInfo = null;
+    $(document).ready(function () {
+        getALLTransactionsByWalletID();
+        getWalletInfoWalletID();
+        console.log(transactions)
+        console.log(walletInfo)
+    });
     $(NOTE_FIELD_LOCATOR).keyup(function () {
 
     });
@@ -47,5 +61,34 @@
 
         });
     }
+
+    function getALLTransactionsByWalletID() {
+        $.ajax({
+            dataType: "json",
+            url: WALLET_TRANSACTIONS_API_URL,
+            data: {'wallet': WALLET_PK},
+            async: false,
+            success: function (result) {
+                transactions = eval(result);
+                // sort by date from most recent
+                transactions.sort((a, b) => {
+                    return new Date(a.date) - new Date(b.date);
+                }).reverse();
+            }
+        });
+    }
+
+    function getWalletInfoWalletID() {
+        $.ajax({
+            dataType: "json",
+            url: WALLET_LIST_API_URL,
+            data: {'id': WALLET_PK},
+            async: false,
+            success: function (result) {
+                walletInfo = eval(result)[0];
+            }
+        });
+    }
+
 
 }
