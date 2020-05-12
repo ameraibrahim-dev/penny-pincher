cdc857258a6a3605f7b84322fad8ae1141b7d734
Brian Xu <201601067@iacademy.edu.ph>
2020-05-12 11:17:21 +0800

    number format

--- penny_pincher/settings/base.py
+++ penny_pincher/settings/base.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # extra build-in
+    'django.contrib.humanize',
 ]
 
 MIDDLEWARE = [
