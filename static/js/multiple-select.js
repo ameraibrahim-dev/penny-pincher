23cdadff0dc6b73e0f67f702c2d9ade96a1f5eec
Lorenzo Ocampo <enzoxo.ocampo7@yahoo.com>
2020-05-12 11:55:10 +0800

    Added event Listeners for checkbox

--- static/js/multiple-select.js
+++ static/js/multiple-select.js
@@ -1,7 +1,14 @@
 var expanded = false;
 
+$(document).ready(function () {
+  let categories
+  categories = isCheckboxChecked();
+  showCheckboxes();
+
+});
+
 function showCheckboxes() {
-  var checkboxes = document.getElementById("checkboxes");
+  let checkboxes = document.getElementById("checkboxes");
 
   if (!expanded) {
     checkboxes.style.display = "block";
@@ -10,4 +17,28 @@ function showCheckboxes() {
     checkboxes.style.display = "none";
     expanded = false;
   }
+}
+
+function isCheckboxChecked(){
+
+  let categories = [];
+
+  $("input[type='checkbox']").click(function () {
+      if(categories.includes($("input[type='checkbox']").attr('id'))){
+         let index = categories.indexOf($(this).attr('id'));
+          categories.splice(index,1);
+      }else {
+
+        //Adds each checked checkbox to array.
+        $.each($("input[type='checkbox']:checked"), function () {
+          if ($(this).attr('id') == undefined) {
+          } else {
+            categories.push($(this).attr('id'));
+          }
+        });
+      }
+    console.log(categories);
+  });
+
+  return categories;
 }
