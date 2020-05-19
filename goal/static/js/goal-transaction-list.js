dbbb0940009e967dd950fd143dc14d70971e0ac1
Brian Xu <201601067@iacademy.edu.ph>
2020-05-19 18:06:05 +0800

    add goal datepicker

--- goal/static/js/goal-transaction-list.js
+++ goal/static/js/goal-transaction-list.js
@@ -1,4 +1,8 @@
-let options = {
+{
+    const GOAL_TRANSACTIONS_API_URL = "/api/v1/user/goal/transactions/list/";
+    const GOAL_PK = $("#goaltID").val();
+    let transactions = [];
+    let options = {
         valueNames: ['name', 'note', 'amount', 'date'],
         page: 10,
         pagination: [{
@@ -9,6 +13,38 @@ let options = {
         }],
     };
 
- let goalList = new List('transactions-list', options);
+
+    function getALLTransactionsByGoalID() {
+        $.ajax({
+            dataType: "json",
+            url: GOAL_TRANSACTIONS_API_URL,
+            data: {'goal': GOAL_PK},
+            async: false,
+            success: function (result) {
+                transactions = result;
+                // sort by date from most recent
+                transactions.sort((a, b) => {
+                    return new Date(a.date) - new Date(b.date);
+                }).reverse();
+            }
+        });
+    }
+
+    $(document).ready(function () {
+        getALLTransactionsByGoalID();
+// set date range
+        console.log(transactions)
+        if (transactions) {
+            $('input[id="DateRangeFilter"]').daterangepicker({
+                endDate: new Date(transactions[0].date),
+                startDate: new Date(transactions.slice(-1)[0].date),
+            })
+        } else {
+            $('input[id="DateRangeFilter"]').daterangepicker({})
+        }
+        let goalList = new List('transactions-list', options);
+    });
+
+}
 
 
