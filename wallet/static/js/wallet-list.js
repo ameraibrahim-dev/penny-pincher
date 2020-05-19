4da23aff268bf3693e45fdd1bb377c81b05250aa
Brian Xu <201601067@iacademy.edu.ph>
2020-05-19 15:26:42 +0800

    remove pagination

--- wallet/static/js/wallet-list.js
+++ wallet/static/js/wallet-list.js
@@ -2,12 +2,7 @@
     let options = {
         valueNames: [{name: 'wallet-type', attr: 'data-attr-wallet-name'},'wallet-type'],
         page: 6,
-        pagination: [{
-            name: "pagination",
-            paginationClass: "pagination",
-            outerWindow: 2,
-            innerWindow: 2,
-        }],
+        pagination: false,
     };
      let wallet_list = new List('wallet-list', options);
 }
